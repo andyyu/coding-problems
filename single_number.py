d06b409ff4888fae4920f517476202d4462c12dd
andyyu <andyhsyu@gmail.com>
2016-05-08 15:42:24 -0700

    added explanation, bit manipulation

--- single_number.py
+++ single_number.py
@@ -5,6 +5,12 @@ Given an array of integers, every element appears twice except for one. Find tha
 Difficulty: Easy - Medium
 
 Solution notes:
+
+This solution uses bit manipulation - bitwise XOR is 0 when both inputs are 1, otherwise it is 1.
+We can use this idea to essentially "cancel out" every duplicate element. 
+If we XOR every element in the array, the elements that appear twice will zero out each other and only the single element will remain.
+
+
 O(n) time
 O(1) space
 
