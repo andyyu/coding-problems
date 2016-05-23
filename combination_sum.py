f238abf150df5401837d35b5c82700fc95b2d42d
andyyu <andyhsyu@gmail.com>
2016-05-23 03:08:29 -0700

    DFS in a nutshell

--- combination_sum.py
+++ combination_sum.py
@@ -17,8 +17,26 @@ A solution set is:
 Difficulty: Medium
 
 Solution notes:
+Use depth-first search. 
+Code is fairly self explanatory, but gist of it is to recursively call dfs, adding 1 more number in candidate to the path, until we reach the target or over it.
+If we reach the target, we add the path to the result, otherwise stop that path.
+Some optimizations made: index was used because we should never add a smaller number than the latest in the path - both for speed and to avoid duplicate paths.
+i.e. when making a path [2, 3] towards a target 7, we should never add another 2. 
+This is why the loop that recursively calls dfs runs from (index, end).
 
-O(n) time 
-O(1) space
+https://leetcode.com/discuss/61491/asked-discuss-time-complexity-your-solution-what-would-say
 '''
+def combination_sum(candidates, target):
+  res = []
+  candidates.sort()
+  dfs(candidates, target, 0, [], res)
+  return res
 
+def dfs(candidates, target, index, path, res):
+  if target == 0:
+    res.append(path)
+    return
+  for i in xrange(index, len(candidates)):
+    if candidates[i] > target:
+      break
+    dfs(candidates, target-candidates[i], i, path + [candidates[i]], res)
